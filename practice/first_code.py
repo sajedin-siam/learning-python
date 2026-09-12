cae168d022f252048b85b7a4ad3d62114f9a7937
Siam <sajedinsiam73@gmail.com>
2026-09-12 11:47:49 +0600

    Add first Python practice script

---
+++ practice/first_code.py
@@ -0,0 +1,71 @@
+print("Hello World")
+print("Sajedin Siam")
+print(23)
+name="Siam"
+age=23
+p=23.99
+
+print(name)
+print(age)
+print(p)
+print("My name is:",name)
+print("My age is :",age)
+
+print(type(name))
+print(type(age))
+print(type(p))  
+
+n1='Siam'
+n2="Siam"
+n3='''Siam'''
+print(n1)
+print(n2)
+print(n3)
+
+old=False
+a=None
+print(type(old))
+print(type(a))
+
+
+
+A=23
+B=23
+sum=A+B
+print(sum)
+
+
+C=26
+D=23
+Diff=C-D
+print(Diff)
+
+
+As=12
+Bs=5
+Cs=As//Bs
+print(Cs)
+
+AD=-12
+BD=5
+CD=AD//BD
+print(CD)
+
+AF=12
+BF=-5
+CF=AF//BF
+print(CF)
+
+#taking input from user
+#string input
+nam=input("Name: ")
+
+#integer onput
+ 
+num=int(input("Age: "))
+
+#float input
+
+price=float(input("Price: "))
+
+print("My  name is ",nam, "I am ",age, "Years old")
