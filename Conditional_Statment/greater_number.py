a4b99cb2c0a45ff9ce449fc12167edb271b02895
Siam <sajedinsiam73@gmail.com>
2026-09-12 16:40:30 +0600

    Add greater number python practice code

---
+++ Conditional_Statment/greater_number.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter 1st number: "))
+num2=int(input("Enter 2nd number: "))
+
+if(num1>num2):
+    print("Number 1 is greater then number 2")
+else:
+    print("number 2 is greater then number  1")
