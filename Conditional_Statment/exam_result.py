c20245c8238ea5482a90707ba53ccb5683263bdf
Siam <sajedinsiam73@gmail.com>
2026-09-13 12:26:22 +0600

    add exam result condition

---
+++ Conditional_Statment/exam_result.py
@@ -0,0 +1,8 @@
+math=int(input("Enter your Math marks:"))
+english=int(input("Enter your english marks:"))
+physics=int(input("Enter your physics marks:"))
+
+if(math>=40 and english>=40 and physics>=40):
+    print("Pass")
+else:
+    print("Fail")
